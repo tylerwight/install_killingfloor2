642a3339b23d1ce662b4d08462dffc744a0fb82c
user <tylermwight@gmail.com>
2021-06-08 05:51:23 +0000

    first commit

---
+++ install_killingfloor2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#Install kf2 dedicated server
+#Tyler Wight
+
+sudo adduser kf2
+sudo usermod -aG sudo kf2
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+#adding multiverse required to install cmd on 64 bit machines
+
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt-get update -y
+sudo apt install lib32gcc1 steamcmd -y
+sudo ln -s /usr/games/steamcmd steamcmd
+sudo mkdir /mnt/kf2
+sudo chown -R kf2:kf2 /mnt/kf2
+sudo chmod 777 /mnt/kf2
+sudo su -c "steamcmd +login anonymous +force_install_dir /mnt/kf2 +app_update 232130 validate +exit" -s /bin/sh root
+
+
